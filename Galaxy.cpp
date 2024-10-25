0a8a60f7e0af2baed4b0f726ae1a22360d87648b
Wayne-dot <wayneting.wc@gmail.com>
2024-10-24 22:45:45 -0400

    update

--- Galaxy.cpp
+++ Galaxy.cpp
@@ -98,6 +98,8 @@ Probe Galaxy::searchProbeByName(const std::string& name)
 
     if (index != -1)
     {
+        cout << endl << "Probe found: " << endl;
+        probs[index].displayProbe();
         return probs[index];
     }
     else
@@ -147,7 +149,11 @@ void Galaxy::swapProbeData(int idx1, int idx2)
     if (idx1 >= 0 && idx1 < probs.size() && idx2 >= 0 && idx2 < probs.size())
     {
         // Use the overloaded '-' operator to swap dimensions, positions, and areas
-        probs[idx1] - probs[idx2];  
+        probs[idx1] - probs[idx2];
+        cout << "Probe at index " << idx1 << ":" << endl;
+        probs[idx1].displayProbe();
+        cout << endl << "Probe at index " << idx2 << ":" << endl;
+        probs[idx2].displayProbe();
     }
     else
     {
@@ -157,25 +163,25 @@ void Galaxy::swapProbeData(int idx1, int idx2)
 
 
 
-void Galaxy::insertProbeData(int galaxyIdx, int probeIdx, int value) {
+void Galaxy::insertProbeData(int probs_index, int dim_pos, int options, int value) {
     // Check for valid galaxy index and probe index
-    if (galaxyIdx < 0 || galaxyIdx >= probs.size() || 
-        probeIdx < 0 || probeIdx > 1) {  // Only two dimensions
+    if (probs_index < 0 || probs_index > 10 || dim_pos > 1 || dim_pos < 0 || options > 1 || options < 0) {  // Only two dimensions
         std::cerr << "Error: Invalid index." << std::endl;
         return;
     }
 
     // Determine if we are inserting into dimensions or positions
-    if (galaxyIdx < 2) {
-        probs[galaxyIdx].setDimension(galaxyIdx, value);
-    } else {
-        probs[galaxyIdx].setPosition(probeIdx, value);
+    if(dim_pos == 0){
+        // dim
+        probs[probs_index].setDimension(options, value);
     }
+    else{
+        probs[probs_index].setPosition(options, value);
 }
 
 void Galaxy::printAllNames(){
     for(int i = 0 ; i < 10 ; i++){
-        cout << i << ". " << probs[i].getName() << endl;
+        cout << i+1 << ". " << probs[i].getName() << endl;
     }
 }
 
